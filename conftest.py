1f297d4f124df8f73f3be4647c8a558ab0eac7de
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-04-23 21:48:08 +0100

    initial work

---
+++ conftest.py
@@ -0,0 +1,21 @@
+"""
+This is a conftest.py file for pytest, which is used to set up fixtures and hooks for testing.
+This file is used to define fixtures that can be used across multiple test files.
+It is also used to define hooks that can be used to modify the behavior of pytest.
+"""
+
+import pytest
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+LOCAL_ENV_PATH = Path(os.getcwd()) / 'local.env'
+
+
+@pytest.fixture(autouse=True, scope="session")
+def import_local_env_file() -> None:
+    """
+    This fixture is used to import the local.env file into the test environment, if the file is present.
+    """
+    if Path.is_file(LOCAL_ENV_PATH):
+        load_dotenv(LOCAL_ENV_PATH, override=False)
