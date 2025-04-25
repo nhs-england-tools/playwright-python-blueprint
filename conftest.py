10ace0fe0541b528c76061420cbb61b3fef1a1ec
Dave Harding <35965578+davethepunkyone@users.noreply.github.com>
2025-04-25 15:21:06 +0100

    Updated logic

--- conftest.py
+++ conftest.py
@@ -15,7 +15,12 @@ LOCAL_ENV_PATH = Path(os.getcwd()) / 'local.env'
 @pytest.fixture(autouse=True, scope="session")
 def import_local_env_file() -> None:
     """
-    This fixture is used to import the local.env file into the test environment, if the file is present.
+    This fixture is used to import the local.env file into the test environment (if the file is present),
+    and will populate the environment variables prior to any tests running.
+    If environment variables are set in a different way when running (e.g. via cli), this will
+    prioritise those values over the local.env file.
+
+    NOTE: You should not use this logic to read a .env file in a pipeline or workflow to set sensitive values.
     """
     if Path.is_file(LOCAL_ENV_PATH):
         load_dotenv(LOCAL_ENV_PATH, override=False)
