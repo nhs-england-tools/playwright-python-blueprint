4062f228cdcdf9b2b69bb5888958306d2a857580
vidhya-chandra1 <198918051+vidhya-chandra1@users.noreply.github.com>
2025-05-15 10:24:52 +0100

    Docstring is newly added for POM - ContactWithPatientPage. (#68)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    
    BCSS - Playwright - Compartment 5 - Docstring creation - POM for
    'Contact with Patient' page
    
    ## Context
    
    BCSS - Playwright - Compartment 5 - Docstring creation - POM for
    'Contact with Patient' page
    
    ## Type of changes
    
    Docstring is Newly created for POM for 'Contact with Patient' page
    
    - [x] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [x] I am familiar with the [contributing
    guidelines](https://github.com/nhs-england-tools/playwright-python-blueprint/blob/main/CONTRIBUTING.md)
    - [x] I have followed the code style of the project
    - [ ] I have added tests to cover my changes (where appropriate)
    - [x] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [x] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- pages/screening_subject_search/contact_with_patient_page.py
+++ pages/screening_subject_search/contact_with_patient_page.py
@@ -24,25 +24,63 @@ class ContactWithPatientPage(BasePage):
         self.save_button = self.page.get_by_role("button", name="Save")
 
     def select_direction_dropdown_option(self, direction: str) -> None:
+        """
+        Select an option from the 'Direction' dropdown by its label.
+
+        Args:
+        direction (str): The label of the direction option to select.
+        """
         self.contact_direction_dropdown.select_option(label=direction)
 
     def select_caller_id_dropdown_index_option(self, index_value: int) -> None:
+        """
+        Select an option from the 'Caller ID' dropdown by its index.
+
+        Args:
+        index_value (int): The index of the caller ID option to select.
+        """
         self.contact_made_between_patient_and_dropdown.select_option(index=index_value)
 
     def click_calendar_button(self) -> None:
+        """Click the 'Calendar' button to open the calendar picker."""
         self.click(self.calendar_button)
 
     def enter_start_time(self, start_time: str) -> None:
+        """
+        Enter a value into the 'Start Time' field.
+
+        Args:
+        start_time (str): The start time to enter into the field.
+        """
         self.start_time_field.fill(start_time)
 
     def enter_end_time(self, end_time: str) -> None:
+        """
+        Enter a value into the 'End Time' field.
+
+        Args:
+        end_time (str): The end time to enter into the field.
+        """
         self.end_time_field.fill(end_time)
 
     def enter_discussion_record_text(self, value: str) -> None:
+        """
+        Enter text into the 'Discussion Record' field.
+
+        Args:
+        value (str): The text to enter into the discussion record field.
+        """
         self.discussion_record_text_field.fill(value)
 
     def select_outcome_dropdown_option(self, outcome: str) -> None:
+        """
+        Select an option from the 'Outcome' dropdown by its label.
+
+        Args:
+        outcome (str): The label of the outcome option to select.
+        """
         self.outcome_dropdown.select_option(label=outcome)
 
     def click_save_button(self) -> None:
+        """Click the 'Save' button to save the contact with patient form."""
         self.click(self.save_button)
