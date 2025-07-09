88d72d1239401ccc3db9daf08cab76e3166a1009
Rory A <rory.aitchison1@nhs.net>
2025-07-09 13:05:39 +0100

    feat: replace hyphens with spaces for markers arg
    -value is now being passed from bs-select-app with hyphens instead of spaces

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 BASE_URL=${1:-${BASE_URL}}
 MARKERS_TO_USE=${2:=${MARKERS_TO_USE}}
+MARKERS_TO_USE="${MARKERS_TO_USE//-/ }"
 export COGNITO_USER_PASSWORD=${3:=${COGNITO_USER_PASSWORD}}
 
 pytest --tracing retain-on-failure --base-url $1 -m "$2"
