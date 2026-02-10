8e702f71329b8ac9fdfb79795c0f14d7460a0c1f
mepr1 <megha.prasannan1@nhs.net>
2026-02-10 14:46:25 +0000

    Feature/bcss 22624 lynchregressiontests scenario 2.1 (#204)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    Migrating scenario 2.1 from selenium to playwright
    
    ## Context
    
    Migrating scenario 2.1 from selenium to playwright
    
    ## Type of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply. -->
    
    - [ ] Refactoring (non-breaking change)
    - [x ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [ x] I am familiar with the [contributing
    guidelines](https://github.com/nhs-england-tools/playwright-python-blueprint/blob/main/CONTRIBUTING.md)
    - [x ] I have followed the code style of the project
    - [x ] I have added tests to cover my changes (where appropriate)
    - [x ] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [x ] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

---
+++ tests/regression/regression_tests/lynch_regression_tests/test_lynch_scenario_2_1.py
@@ -0,0 +1,317 @@
+import logging
+import pytest
+from playwright.sync_api import Page
+from classes.repositories.general_repository import GeneralRepository
+from classes.repositories.subject_repository import SubjectRepository
+from pages.base_page import BasePage
+from pages.logout.log_out_page import LogoutPage
+from pages.screening_practitioner_appointments.appointment_detail_page import (
+    AppointmentDetailPage,
+)
+
+from pages.screening_subject_search.episode_events_and_notes_page import (
+    EpisodeEventsAndNotesPage,
+)
+from pages.screening_subject_search.subject_screening_summary_page import (
+    SubjectScreeningSummaryPage,
+)
+from utils import screening_subject_page_searcher
+from utils.appointments import  book_appointments
+from utils.batch_processing import batch_processing
+from utils.lynch_utils import LynchUtils
+from utils.oracle.oracle import OracleDB
+from utils.subject_assertion import subject_assertion
+from utils.user_tools import UserTools
+
+@pytest.mark.usefixtures("setup_org_and_appointments")
+@pytest.mark.vpn_required
+@pytest.mark.regression
+@pytest.mark.lynch_regression_tests
+def test_lynch_scenario_2_1(page: Page) -> None:
+    """
+    Scenario: 2.1 - Screening Centre discharges patient prior to colonoscopy assessment - self referral
+
+    G4-G2-G3-A183-A25-J24-J25-J26-C203 above age range [SSCL5b(J26)]
+
+    This scenario tests where the screening centre discharges the patient before the colonoscopy assessment appointment takes place.
+
+    Subject summary:
+
+    > Process Lynch diagnosis for a new over-age subject suitable for immediate self-referral
+    > Self refer the subject
+    > Run Lynch invitations > G4 (5.1)
+    > Process G4 letter batch > G2 (5.1)
+    > Run timed events > G3 (5.1)
+    > Book appointment > A183 (1.11)
+    > Process A183 letter batch > A25 (1.11)
+    > SC discharges patient > J24 (1.11)
+    > Process J24 letter batch > J25 (1.11)
+    > Process J25 letter batch > J26 (1.11)
+    > Check recall [SSCL5b(J26)]
+    """
+    # Given I log in to BCSS "England" as user role "Hub Manager"
+    user_role = UserTools.user_login(
+        page, "Hub Manager State Registered at BCS01", return_role_type=True
+    )
+    if user_role is None:
+        raise ValueError("User cannot be assigned to a UserRoleType")
+
+    # When I receive Lynch diagnosis "EPCAM" for a new subject in my hub aged "75" with diagnosis date "3 years ago" and last colonoscopy date "2 years ago"
+    nhs_no = LynchUtils(page).insert_validated_lynch_patient_from_new_subject_with_age(
+        age="75",
+        gene="EPCAM",
+        when_diagnosis_took_place="3 years ago",
+        when_last_colonoscopy_took_place="2 years ago",
+        user_role=user_role,
+    )
+
+    # Then Comment: NHS number
+    assert nhs_no is not None
+    logging.info(f"[SUBJECT CREATION] Created Lynch subject with NHS number: {nhs_no}")
+
+    # When I self refer the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
+    SubjectScreeningSummaryPage(page).click_self_refer_button()
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "Calculated FOBT due date": "Null",
+            "Calculated lynch due date": "Today",
+            "Calculated surveillance due date": "Null",
+            "Lynch due date": "Today",
+            "Lynch due date date of change": "Null",
+            "Lynch due date reason": "Self-referral",
+            "Previous screening status": "Lynch Surveillance",
+            "Screening due date": "Null",
+            "Screening due date date of change": "Null",
+            "Screening due date reason": "Null",
+            "Subject has lynch diagnosis": "Yes",
+            "Subject lower FOBT age": "Default",
+            "Subject lower lynch age": "25",
+            "Screening status": "Lynch Self-referral",
+            "Screening status date of change": "Today",
+            "Screening status reason": "Self-referral",
+            "Subject age": "75",
+            "Surveillance due date": "Null",
+            "Surveillance due date date of change": "Null",
+            "Surveillance due date reason": "Null",
+        },
+    )
+
+    # When I set the Lynch invitation rate for all screening centres to 50
+    LynchUtils(page).set_lynch_invitation_rate(rate=50)
+
+    # And I run the Lynch invitations process
+    GeneralRepository().run_lynch_invitations()
+
+    # And my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest episode type": "Lynch Surveillance",
+            "latest episode sub-type": "Over Age",
+            "latest event status": "G4 Selected for Lynch Surveillance (Self-referral)",
+            "calculated fobt due date": "Null",
+            "calculated lynch due date": "Today",
+            "calculated surveillance due date": "Null",
+            "lynch due date": "Today",
+            "lynch due date date of change": "Null",
+            "lynch due date reason": "Self-referral",
+            "previous screening status": "Lynch Surveillance",
+            "screening due date": "Null",
+            "screening due date date of change": "Null",
+            "screening due date reason": "Null",
+            "subject has lynch diagnosis": "Yes",
+            "subject lower fobt age": "Default",
+            "subject lower lynch age": "25",
+            "screening status": "Lynch Self-referral",
+            "screening status date of change": "Today",
+            "screening status reason": "Self-referral",
+            "subject age": "75",
+            "surveillance due date": "Null",
+            "surveillance due date date of change": "Null",
+            "surveillance due date reason": "Null",
+        },
+    )
+
+    # And there is a "G4" letter batch for my subject with the exact title "Lynch Surveillance Invitation (Self-referral)"
+    # When I process the open "G4" letter batch for my subject
+    batch_processing(
+        page,
+        "G4",
+        "Lynch Surveillance Invitation (Self-referral)",
+    )
+
+    # Then my subject has been updated as follows:
+    subject_assertion(nhs_no, {"latest event status": "G2 Lynch Pre-invitation Sent"})
+
+    # When I run Timed Events for my subject
+    OracleDB().exec_bcss_timed_events(nhs_number=nhs_no)
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "G3 Lynch Surveillance Colonoscopy Assessment Appointment Required"
+        },
+    )
+
+    # When Comment:
+    logging.info("Progress the episode through the required pathway")
+
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
+
+    # And I view the practitioner appointment booking screen
+    SubjectScreeningSummaryPage(page).click_book_practitioner_clinic_button()
+
+    # And I select "BCS001" as the screening centre where the practitioner appointment will be held
+    # And I set the practitioner appointment date to "today"
+    # And I book the "earliest" available practitioner appointment on this date
+
+    book_appointments(
+        page,
+        "BCS001 - Wolverhampton Bowel Cancer Screening Centre",
+        "The Royal Hospital (Wolverhampton)",
+    )
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A183 1st Colonoscopy Assessment Appointment Requested",
+        },
+    )
+
+    # And there is a "A183" letter batch for my subject with the exact title "Practitioner Clinic 1st Appointment (Lynch)"
+    # When I process the open "A183" letter batch for my subject
+    batch_processing(page, "A183", "Practitioner Clinic 1st Appointment (Lynch)")
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A25 1st Colonoscopy Assessment Appointment Booked, letter sent",
+        },
+    )
+
+    # When I switch users to BCSS "England" as user role "Screening Centre Manager"
+    LogoutPage(page).log_out(close_page=False)
+    BasePage(page).go_to_log_in_page()
+    user_role = UserTools.user_login(page, "Screening Centre Manager at BCS001", True)
+    if user_role is None:
+        raise ValueError("User cannot be assigned to a UserRoleType")
+
+    # And I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
+
+    # And I view the event history for the subject's latest episode
+    SubjectScreeningSummaryPage(page).expand_episodes_list()
+    SubjectScreeningSummaryPage(page).click_first_lynch_surveillance_episode_link()
+
+    # And I view the latest practitioner appointment in the subject's episode
+    EpisodeEventsAndNotesPage(page).click_most_recent_view_appointment_link()
+
+    # And The Screening Centre cancels the practitioner appointment with reason "Patient Unsuitable - Recently Screened"
+    AppointmentDetailPage(page).check_cancel_radio()
+    AppointmentDetailPage(page).select_reason_for_cancellation_option(
+        "Patient Unsuitable - Recently Screened"
+    )
+
+    # And I press OK on my confirmation prompt
+    AppointmentDetailPage(page).click_save_button(accept_dialog=True)
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "J24 Screening Centre Discharge Patient ",
+        },
+    )
+
+    # And there is a "J24" letter batch for my subject with the exact title "Subject Discharge (Screening Centre) (Lynch)"
+    # When I process the open "J24" letter batch for my subject
+    batch_processing(
+        page,
+        "J24",
+        "Subject Discharge (Screening Centre) (Lynch)",
+    )
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "J25 Patient discharge sent (Screening Centre discharge patient)",
+        },
+    )
+
+    # And there is a "J25" letter batch for my subject with the exact title "GP Discharge (Discharged By Screening Centre) (Lynch)"
+    SubjectRepository().there_is_letter_batch_for_subject(
+        nhs_no,
+        "J25",
+        "GP Discharge (Discharged By Screening Centre) (Lynch)",
+    )
+
+    #    When I switch users to BCSS "England" as user role "Hub Manager"
+    LogoutPage(page).log_out(close_page=False)
+    BasePage(page).go_to_log_in_page()
+    user_role = UserTools.user_login(
+        page, "Hub Manager State Registered at BCS01", return_role_type=True
+    )
+    if user_role is None:
+        raise ValueError("User cannot be assigned to a UserRoleType")
+
+    # When I process the open "J25" letter batch for my subject
+    batch_processing(
+        page,
+        "J25",
+        "GP Discharge (Discharged By Screening Centre) (Lynch)",
+    )
+
+    # Then Comment:
+    logging.info("Check subject details against closure scenario SSCL5b(J26) ")
+
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "Calculated FOBT due date": "Null",
+            "Calculated lynch due date": "2 years from latest J25 event",
+            "Calculated surveillance due date": "Null",
+            "Ceased confirmation date": "Today",
+            "Ceased confirmation details": "Outside screening population at recall.",
+            "Ceased confirmation user ID": "User's ID",
+            "Clinical reason for cease": "Null",
+            "Latest episode accumulated result": "Lynch non-participation",
+            "Latest episode recall calculation method": "Date of last patient letter",
+            "Latest episode recall episode type": "Lynch Surveillance",
+            "Latest episode recall surveillance type": "Null",
+            "Latest episode status": "Closed",
+            "Latest episode status reason": "Discharged",
+            "Latest event status": "J26 GP Discharge letter sent (Discharge by Screening centre)",
+            "Lynch due date": "Null",
+            "Lynch due date date of change": "Today",
+            "Lynch due date reason": "Ceased",
+            "Lynch incident episode": "Null",
+            "Screening due date": "Null",
+            "Screening due date date of change": "Null",
+            "Screening due date reason": "Null",
+            "Screening status": "Ceased",
+            "Screening status date of change": "Today",
+            "Screening status reason": "Outside Screening Population",
+            "Surveillance due date": "Null",
+            "Surveillance due date date of change": "Unchanged",
+            "Surveillance due date reason": "Unchanged",
+        },
+        user_role,
+    )
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
+    # Then I "can" see a button with value of "Self-refer Lynch Surveillance"
+    SubjectScreeningSummaryPage(page).button_with_value_present(
+        "Self-refer Lynch Surveillance", True
+    )
+
+    LogoutPage(page).log_out()
